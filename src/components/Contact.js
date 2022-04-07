2ee9c489ef4423ad6c9a6244c582aa0ae1688ece
LucaPalminteri <lucapalminteri02@gmail.com>
2022-04-07 20:09:50 -0300

    Finished first version of the portfolio

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -6,6 +6,10 @@ import {faEnvelope} from "@fortawesome/free-solid-svg-icons"
 
 
 export default function Contact() {
+
+    const d = new Date();
+    let year = d.getFullYear();
+
     return (
         <div className="contact subtitle">
             <h2>contact.</h2>
@@ -13,20 +17,24 @@ export default function Contact() {
                 <form className="container--form">
                     <input className="name input" type="text" placeholder="Name" required></input>
                     <input className="email input" type="email" placeholder="Email" required></input>
-                    <input className="message input" type="textarea" placeholder="Message" required></input>
+                    <textarea className="message input" type="textarea" placeholder="Message" required></textarea>
                     <button className="send input" type="submit">Send</button>
                 </form>
                 <div className="container--info">
-                    <h3>Contact information</h3>
-                    <p>I'm open for any suggestion or just have a chat</p>
-                    <h4>Email: lucapalminteri02@gmail.com</h4>
-                    <div className="info--social">
-                        <a href="https://www.linkedin.com/in/luca-palminteri/?locale=en_US" target="_blank"><FontAwesomeIcon className="icon" icon={faLinkedinIn}/></a>
-                        <a href="https://github.com/LucaPalminteri" target="_blank"><FontAwesomeIcon className="icon" icon={faGithub}/></a>
-                        <a href="mailto:lucapalminteri02@gmail.com" target="_blank"><FontAwesomeIcon className="icon" icon={faEnvelope}/></a>
-                    </div>
+                    <h3>I would like to hear from you.</h3>
                 </div>
+                {/* <a href="https://www.linkedin.com/in/luca-palminteri/?locale=en_US" target="_blank"><FontAwesomeIcon className="icon" icon={faLinkedinIn}/></a>
+                        <a href="https://github.com/LucaPalminteri" target="_blank"><FontAwesomeIcon className="icon" icon={faGithub}/></a>
+                        <a href="mailto:lucapalminteri02@gmail.com" target="_blank"><FontAwesomeIcon className="icon" icon={faEnvelope}/></a> */}
             </div>
+            <div className="contact__social">
+                <a href="https://www.linkedin.com/in/luca-palminteri/?locale=en_US" target="_blank"><FontAwesomeIcon className="icon" icon={faLinkedinIn}/></a>
+                <a href="https://github.com/LucaPalminteri" target="_blank"><FontAwesomeIcon className="icon" icon={faGithub}/></a>
+                <a href="mailto:lucapalminteri02@gmail.com" target="_blank"><FontAwesomeIcon className="icon" icon={faEnvelope}/></a>
+            </div>
+            <footer className="contact__footer">
+                <h4>Copyright &copy; {year} Luca Palminteri - All Rights Reserved</h4>
+            </footer>
         </div>
     )
 }
